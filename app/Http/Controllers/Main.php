c81ff4eeabb0ec0fb6b85d971f9343033ae8302f
Getúlio Werle <getuliowerle10@gmail.com>
2024-12-31 10:46:59 -0400

    created the login validation

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -24,12 +25,34 @@ class Main extends Controller
         ];
         return view('login_frm', $data);
     }
-    
-    public function login_submit()
+
+    public function login_submit(Request $request)
     {
-        // fake login
-        session()->put('username', 'admin');
-        echo 'logado';
+        // form validation
+        $request->validate([
+            'text_username' => 'required|min:3',
+            'text_password' => 'required|min:3'
+        ], [
+            'text_username.required' => 'The field is mandatory',
+            'text_username.min' => 'The field must have at least 3 characters',
+            'text_password.required' => 'The field is mandatory',
+            'text_password.min' => 'The field must have at least 3 characters'
+        ]);
+        // get login data
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+        // check if user exists and your password is correct
+        $model = new UserModel();
+        $user_data = $model->where('username', '=', $username)->whereNull('deleted_at')->first();
+        if ($user_data && password_verify($password, $user_data->password)) {
+            $session_data = [
+                'id' => $user_data->id,
+                'username' => $user_data->username
+            ];
+            session()->put($session_data);
+            return redirect()->route('index');
+        }
+        return redirect()->route('login')->withInput()->with('login_error', 'Invalid login');
     }
 
     // logout
--- resources/views/login_frm.blade.php
+++ resources/views/login_frm.blade.php
@@ -9,16 +9,25 @@
             @csrf
             <div class="mb-3">
                 <label for="text_username" class="form-label">Username</label>
-                <input type="text" name="text_username" id="text_username" class="form-control" placeholder="Username" required>
+                <input type="text" name="text_username" id="text_username" class="form-control" placeholder="Username" required value="{{ old('text_username') }}">
+                @error('text_username')
+                    <div class="text-danger">{{ $errors->get('text_username')[0] }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="text_password" class="form-label">Username</label>
-                <input type="password" name="text_password" id="text_password" class="form-control" placeholder="Passoword" required>
+                <input type="password" name="text_password" id="text_password" class="form-control" placeholder="Passoword" required value="{{ old('text_password') }}">
+                @error('text_password')
+                    <div class="text-danger">{{ $errors->get('text_password')[0] }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <button type="submit" class="btn btn-primary w-100">Login</button>
             </div>
         </form>
+        @if(session()->has('login_error'))
+            <div class="alert alert-danger">{{ session()->get('login_error') }}</div>
+        @endif
     </div>
 </div>
 
